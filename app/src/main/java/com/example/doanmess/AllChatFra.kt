18129d6abb558db37447946de3bfbded3bc01342
TNMonster <nhatmonsterhack@gmail.com>
2024-10-11 00:19:56 +0700

    commit

--- app/src/main/java/com/example/doanmess/AllChatFra.kt
+++ app/src/main/java/com/example/doanmess/AllChatFra.kt
@@ -6,15 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 
 class AllChatFra : Fragment() {
     // TODO: Rename and change types of parameters
-    private var list: List<DataMess> = listOf()
+    private var list: MutableList<DataMess> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        list = listOf(
+        list = mutableListOf(
             DataMess(R.drawable.avatar_placeholder_allchat, "Nguyễn Văn A", "Hello", "10:00", true, false),
             DataMess(R.drawable.avatar_placeholder_allchat, "Nguyễn Văn B", "Hi", "10:00", false, true),
             DataMess(R.drawable.avatar_placeholder_allchat, "Nguyễn Văn C", "Hello", "10:00", false, true),
@@ -31,6 +32,7 @@ class AllChatFra : Fragment() {
             DataMess(R.drawable.avatar_placeholder_allchat, "Nguyễn Văn N", "Hi", "10:00", true, true),
             DataMess(R.drawable.avatar_placeholder_allchat, "Nguyễn Văn O", "Hello", "10:00", false, false),
         )
+        list.add(DataMess(R.drawable.avatar_placeholder_allchat, "Nguyễn Văn P", "Hi", "10:00", false, true))
     }
 
     override fun onCreateView(
@@ -38,7 +40,7 @@ class AllChatFra : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view= inflater.inflate(R.layout.fragment_all_chat, container, false)
-        val recyclerView = view.findViewById<androidx.recyclerview.widget.RecyclerView>(R.id.RVChat_AllChat)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.RVChat_AllChat)
         val adapter = Chat_AllChatAdapter(list)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
