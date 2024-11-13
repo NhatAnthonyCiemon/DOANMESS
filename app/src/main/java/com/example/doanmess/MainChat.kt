2c783672f050aa89d19da7959544fe512fb34f6f
TNMonster <nhatmonsterhack@gmail.com>
2024-11-13 10:15:43 +0700

    sua

--- app/src/main/java/com/example/doanmess/MainChat.kt
+++ app/src/main/java/com/example/doanmess/MainChat.kt
@@ -10,6 +10,8 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -51,7 +53,11 @@ class MainChat : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.main_chat)
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top , systemBars.right, systemBars.bottom)
+            insets
+        }
         auth = Firebase.auth
         val currentUserUid = auth.currentUser?.uid
         val targetUserUid = intent.getStringExtra("uid") ?: return
